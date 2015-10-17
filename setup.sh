953432386a7ff1aaf91f4a800f8d8b59e21c3c23
Will Richardson <will@grinful.com>
2015-10-17 11:24:14 -0500

    added setup file

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+# initial pull just for refence
+# git clone https://github.com/willr/dot_files.git .dot_files
+
+# setup links to for config files
+ln -s ~/.dot_files/.tmux.conf ~/.tmux.conf
+
+
